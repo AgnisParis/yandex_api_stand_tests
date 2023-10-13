a4884bdb88650950a13d376e35ff13157e7d07a9
Агнетта Сухарева <agnettasuhareva@MacBook-Pro-Agnetta.local>
2023-10-13 13:51:38 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://633d293b-6567-4313-a6b5-dccbbb56033b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from math import expm1
+from math import copysign as v
+import data
+
+print(data.user_body)
+
+x = 3
+y = -4
+print(expm1(x))
+print(v(x, y))
+module_name = len("string")
+print(module_name)
+print(sum((int(input()), int(input()))))
+
+
+def input_members_names(count_of_members: int):
+    pass
+
+
+# print(repr(string))
+
+
+def __innit__():
+    print("hello")
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
